b8cabc49838eddcfc5ad9faef6633419a7aa6a9a
Valotto18 <rm551445@fiap.com.br>
2025-06-09 10:49:38 -0300

    Atualizações...

--- Blackoutguard/BlackoutGuard/Program.cs
+++ Blackoutguard/BlackoutGuard/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BlackoutGuard.UI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var app = new MainApp();
+        app.Iniciar();
+    }
+}
